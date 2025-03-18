0c992540327d7a22b1ca82c975f07c474ebc1ccb
Anarox <68535747+Anarox1111@users.noreply.github.com>
2025-03-18 21:52:24 +0100

    fixed eventhandler

--- game/eventhandler.js
+++ game/eventhandler.js
@@ -1,5 +1,5 @@
 import { Tower, towers } from "../entities/tower.js";
-import { canvas, money, price, updateMoney, updateResources, towerDamageElement, towerUpgradePriceElement, moneyElement } from "./game.js";
+import { canvas, money, price, updateMoney, updateResources, updateTowerStats } from "./game.js";
 import { cellSize } from "./grid.js";
 
 export const mouse = {
