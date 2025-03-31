d82d13795f684484e4d3263a51abd1ccfec0ed71
GitGudree <117321184+GitGudree@users.noreply.github.com>
2025-03-31 16:33:14 +0200

    default fix
    not selecting a tower and placing one
    now results in a basic tower being placed not the error tower

--- entities/towers/towerState.js
+++ entities/towers/towerState.js
@@ -1,11 +1,11 @@
 /**
- * class used to get input from script in index
+ * class used to get input from script in main the basic blue tower is default
  *               
  * @author:    Quetzalcoatl
  * Created:   31.03.2025
  **/
 
-let chosenTower ="";
+let chosenTower ="basic";
 export function setChosenTower(typeIn){
     chosenTower = typeIn
     console.log("valgte tower" +" "+ chosenTower)
