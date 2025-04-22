38adbacadaa8eef912b83c9905f3e7b8a9d39da1
GitGudree <117321184+GitGudree@users.noreply.github.com>
2025-04-22 15:52:07 +0200

    barricade upgrade fix

--- entities/towers/Barricade.js
+++ entities/towers/Barricade.js
@@ -23,7 +23,91 @@ export class Barricade extends Tower {
         Barricade.splice(Barricade.indexOf(this), 1);
     }
 
-    attack(){};
-    upgrade() {}
-    getUpgradeStats() {}
+    attack() {};
+    upgrade() {
+                if (money < this.upgradeCost || this.upgradeCost === -1) return;
+        
+                // DO NOT REMOVE THIS CODE!!!
+                // const towerUpgrades = towerTypes['Shooter'].upgradePath;
+        
+                // for (let upgradeKey in towerUpgrades[this.upgrades]) {
+                //     const upgrade = towerUpgrades[upgradeKey];
+                //     this[upgradeKey] = upgrade[upgradeKey];
+                // }
+        
+                const cost = this.upgradeCost;
+                switch (this.upgrades) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        return;
+                }
+        
+                updateMoney('decrease', cost);
+        
+                this.health += 50;
+                this.upgrades++;
+                
+                
+                //towerDamageElement.textContent = this.damage;
+                //towerUpgradePriceElement.textContent = this.upgradeCost;
+        
+            }
+            
+            /**
+             * getUpgradeStats
+             *
+        
+            * @description Two objects, { old ... new } The new object is an instance of the old one, and are further tweaked to use newer upgrade stats,
+            * serves as a temporarily data-placeholder for adding additional objects before project structure will be rewritten.
+            * Author:    Anarox
+            * Created:   09.03.2025
+            **/
+            getUpgradeStats() {
+        
+                const oldStats = {
+                    health: this.health,
+                    range: this.range,
+                    fireRate: this.fireRate,
+                    damage: this.damage,
+                    upgradeCost: this.upgradeCost
+                };
+        
+                let newRange = this.range;
+                let newFireRate = this.fireRate;
+                let newDamage = this.damage;
+                let newUpgradeCost = this.upgradeCost;
+        
+                switch (this.upgrades) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        return {
+                            oldStats,
+                            newStats: oldStats
+                        };
+                }
+        
+                const newStats = {
+                    health: oldStats.health + 50,
+                    range: newRange,
+                    fireRate: newFireRate,
+                    damage: newDamage,
+                    upgradeCost: newUpgradeCost
+                };
+        
+                return { oldStats, newStats };
+            }
 }
--- entities/towers/ErrorTower.js
+++ entities/towers/ErrorTower.js
@@ -1,4 +1,5 @@
 import { Tower } from "./tower.js";
+import { money, updateMoney } from "../../game/game.js";
 /**
  * Error tower class used only if the towerFactory gets an incorrect input and thus uses the default
  *
@@ -19,6 +20,90 @@ export class ErrorTower extends Tower {
         this.bulletType = type;
         this.background = "black";
     }
-    upgrade() {}
-    getUpgradeStats() {}
+    upgrade() {
+        if (money < this.upgradeCost || this.upgradeCost === -1) return;
+
+        // DO NOT REMOVE THIS CODE!!!
+        // const towerUpgrades = towerTypes['Shooter'].upgradePath;
+
+        // for (let upgradeKey in towerUpgrades[this.upgrades]) {
+        //     const upgrade = towerUpgrades[upgradeKey];
+        //     this[upgradeKey] = upgrade[upgradeKey];
+        // }
+
+        const cost = this.upgradeCost;
+        switch (this.upgrades) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                return;
+        }
+
+        updateMoney('decrease', cost);
+
+        this.health += 50;
+        this.upgrades++;
+        
+        
+        //towerDamageElement.textContent = this.damage;
+        //towerUpgradePriceElement.textContent = this.upgradeCost;
+
+    }
+    
+    /**
+     * getUpgradeStats
+     *
+
+    * @description Two objects, { old ... new } The new object is an instance of the old one, and are further tweaked to use newer upgrade stats,
+    * serves as a temporarily data-placeholder for adding additional objects before project structure will be rewritten.
+    * Author:    Anarox
+    * Created:   09.03.2025
+    **/
+    getUpgradeStats() {
+
+        const oldStats = {
+            health: this.health,
+            range: this.range,
+            fireRate: this.fireRate,
+            damage: this.damage,
+            upgradeCost: this.upgradeCost
+        };
+
+        let newRange = this.range;
+        let newFireRate = this.fireRate;
+        let newDamage = this.damage;
+        let newUpgradeCost = this.upgradeCost;
+
+        switch (this.upgrades) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                return {
+                    oldStats,
+                    newStats: oldStats
+                };
+        }
+
+        const newStats = {
+            health: oldStats.health + 50,
+            range: newRange,
+            fireRate: newFireRate,
+            damage: newDamage,
+            upgradeCost: newUpgradeCost
+        };
+
+        return { oldStats, newStats };
+    }
 }
