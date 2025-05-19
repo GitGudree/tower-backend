fdfb10a52c174dc1e4be780db0296e909929d9cb
GitGudree <117321184+GitGudree@users.noreply.github.com>
2025-05-19 17:48:16 +0200

    fix sprite imports in loader

--- entities/spriteLoader.js
+++ entities/spriteLoader.js
@@ -9,19 +9,19 @@
 export const sprites = {};
 
 const imagePaths = {
-  gatling: "./public/sprites/gatling-0001-Sheet.png",
-  sniper: "./public/sprites/railgun-0002-Sheet.png",
-  default: "./public/sprites/default-0002-Sheet.png",
-  rocket: "./public/sprites/mortard-0002-Sheet.png",
-  laser: "./public/sprites/lazoar-0001-Sheet.png",
-  artillery: "./public/sprites/artyTower-0001-Sheet.png",
-  barricade:  "./public/sprites/barricade-0001-Sheet.png",
-  slowTower: "./public/sprites/slow-0001.png",
-  mineTower:  "./public/sprites/mine-0001-Sheet.png",
-  enemy: "./public/sprites/normEnemy-0001-Sheet.png",
-  tankEnemy: "./public/sprites/btrEnemy-0001-Sheet.png",
-  bossEnemy: "./public/sprites/mechEnemy-0001-Sheet.png",
-  fastEnemy: "./public/sprites/fastEnemy-0001-Sheet.png"
+  gatling: "/sprites/gatling-0001-Sheet.png",
+  sniper: "/sprites/railgun-0002-Sheet.png",
+  default: "/sprites/default-0002-Sheet.png",
+  rocket: "/sprites/mortard-0002-Sheet.png",
+  laser: "/sprites/lazoar-0001-Sheet.png",
+  artillery: "/sprites/artyTower-0001-Sheet.png",
+  barricade:  "/sprites/barricade-0001-Sheet.png",
+  slowTower: "/sprites/slow-0001.png",
+  mineTower:  "/sprites/mine-0001-Sheet.png",
+  enemy: "/sprites/normEnemy-0001-Sheet.png",
+  tankEnemy: "/sprites/btrEnemy-0001-Sheet.png",
+  bossEnemy: "/sprites/mechEnemy-0001-Sheet.png",
+  fastEnemy: "/sprites/fastEnemy-0001-Sheet.png"
   
 };
 
